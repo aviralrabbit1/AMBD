4d34340aaf2a7a62ff5db9081b9cbfcb131a3d82
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-06 04:36:05 +0530

    Carousel added

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,16 +1,36 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import './Home.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+// taken from https://www.npmjs.com/package/react-responsive-carousel
+
+import {Carousel} from 'react-responsive-carousel';
+
 
 function Home() {
 
-    useEffect(() => {
-      fetch("https://api.themoviedb.org/3/movie/11?api_key=abb4f6428bc3f6a5ee5f4d7de2c21bc3")
-      .then(res => res.json())
-      .then(data => console.log(data))
-    }, [])
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/tv/popular?api_key=abb4f6428bc3f6a5ee5f4d7de2c21bc3&language=en-US")
+    .then(res => res.json())
+    .then(data => setPopularMovies(data.results))
+  }, [])
+  console.log(popularMovies);
     
   return (
     <div>
-        Home Page
+        <div className='poster'>
+          <Carousel 
+            showThumbs={false} autoPlay={true} infiniteLoop={true}
+            showStatus={false} transitionTime={3}
+          >
+            {
+              popularMovies.map(movie => (
+                <span key={movie.id}>{movie.original_name} </span>
+              ))
+            }
+          </Carousel>
+        </div>
     </div>
   )
 }
