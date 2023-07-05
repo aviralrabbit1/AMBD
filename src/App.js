fcd22fe7ec71a96bec28a5b10e4e3123a3e1376b
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-06 01:59:29 +0530

    Added movie and error routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <Router>
         <Routes>
           <Route index element={<h1>AMDB app</h1>} ></Route>
+          <Route path="movie/:id" element={<h1>Movie details</h1>} ></Route>
+          <Route path="movie/:type" element={<h1>Movie list page</h1>} ></Route>
+          <Route path="/*" element={<h1>Error page</h1>} ></Route>
         </Routes>
       </Router>
     </div>
