ca6a5d6d4340e4b06dee17b8f23fb1c61bd678ef
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-08 01:31:54 +0530

    made header responsive

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -29,6 +29,7 @@ const Cards = ({movie}) => {
                 <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
                 <div className="cards__overlay">
                     <div className="card__title">{movie?movie.original_title:""}</div>
+                    {/* <div className="card__rating" >{movie.popularity}</div> */}
                     <div className="card__runtime">
                         {movie?movie.release_date:""}
                         <span className="card__rating">{movie?movie.vote_average:""}<i className="fas fa-star" /></span>
