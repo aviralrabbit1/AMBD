338cb21639826ded55012fab2f91a8b970d3b671
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-08 01:21:10 +0530

    fixed loading skeleton

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from "react"
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import "./Card.css"
-import { Link } from "react-router-dom"
+import React, {useEffect, useState} from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./Card.css";
+import { Link } from "react-router-dom";
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const Cards = ({movie}) => {
 
