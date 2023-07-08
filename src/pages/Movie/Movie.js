b7e08e2c9a598962ddecfd29f2ea669704b68755
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-08 17:45:31 +0530

    added media responsive movie-links

--- src/pages/Movie/Movie.js
+++ src/pages/Movie/Movie.js
@@ -72,27 +72,23 @@ const Movie = () => {
                 </div>
             </div>
             <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                <div className="movie__heading__links">
-                    {
-                        currentMovieDetail && currentMovieDetail.homepage 
-                        && <a href={currentMovieDetail.homepage} target="_blank" 
-                        style={{textDecoration: "none"}}>
-                            <p><span className="movie__homeButton movie__Button">Homepage 
-                            <i className="newTab fas fa-external-link-alt"></i>
-                            </span></p></a>
-                    }
-                </div>
-                <div className="movie__heading__links">
-                    {
-                        currentMovieDetail && currentMovieDetail.imdb_id 
-                        && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} 
-                        target="_blank" style={{textDecoration: "none"}}>
-                            <p><span className="movie__imdbButton movie__Button">IMDb
-                            <i className="newTab fas fa-external-link-alt"></i>
-                            </span></p></a>
-                    }
-                </div>
+                <span className="movie__heading">Useful Links</span>
+                {
+                    currentMovieDetail && currentMovieDetail.homepage 
+                    && <a href={currentMovieDetail.homepage} 
+                    target="_blank" style={{textDecoration: "none"}}>
+                        <p><span className="movie__homeButton movie__Button">Homepage 
+                        <i className="newTab fas fa-external-link-alt"></i>
+                        </span></p></a>
+                }
+                {
+                    currentMovieDetail && currentMovieDetail.imdb_id 
+                    && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} 
+                    target="_blank" style={{textDecoration: "none"}}>
+                        <p><span className="movie__imdbButton movie__Button">IMDb
+                        <i className="newTab fas fa-external-link-alt"></i>
+                        </span></p></a>
+                }
             </div>
             <div className="movie__heading">Production companies</div>
             <div className="movie__production">
